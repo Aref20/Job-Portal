a7eeb4d2cb72c0e9fc8b339d991d6c75d098ceea
Aref Al Hamad <AAlHamad@msgroup.local>
2022-06-29 17:53:43 +0300

    add featuers

--- job/admin.py
+++ job/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
-from job.models import Job, Department , Location , Nature 
+from job.models import *
 
 
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
+#class Department_Personline(admin.TabularInline):
+   # model = Department_Person
+
+
 class jobadmin(SummernoteModelAdmin , admin.ModelAdmin):
     # displaying posts with title slug and created time
-    list_display = ('title', 'department','nature','status', 'post_date')
-    list_filter = ("title",'department', 'post_date','status','nature','location','salary','experience_min','experience_max')
+
+    list_display = ('title','nature','status', 'post_date')
+    list_filter = ("title", 'post_date','status','nature','location','salary','experience_min','experience_max')
     search_fields = ['title', 'description']
     # prepopulating slug from title
     #prepopulated_fields = {'slug': ('title', )}
     summernote_fields = ('description', )
+
+#class DepartmentPersonadmin( admin.ModelAdmin):
+   #     inlines = [Department_Personline,]
 
 
 
@@ -21,8 +29,9 @@ class jobadmin(SummernoteModelAdmin , admin.ModelAdmin):
 
   
 admin.site.register(Job, jobadmin)
-admin.site.register(Department)
+#admin.site.register(Department)
 admin.site.register(Location)
 admin.site.register(Nature)
+#admin.site.register(Department,DepartmentPersonadmin)
 
 
--- job/models.py
+++ job/models.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
 
@@ -17,7 +18,8 @@ class Status(Enum):
 class Job(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(max_length=3000)
-    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+    department = models.ForeignKey(Group,on_delete=models.SET_NULL, null=True) 
+    #DepartmentPerson = models.ForeignKey('Department_Person', on_delete=models.SET_NULL, null=True)
     post_date = models.DateField(auto_now_add=True)
     vacancy = models.IntegerField(blank=True)
     salary = models.IntegerField( blank=True)
@@ -32,8 +34,10 @@ class Job(models.Model):
 
     
 
-class Department(models.Model):
-    department_name = models.CharField(max_length=300)
+#class Department(models.Model):
+  #  department_name = models.CharField(max_length=300)
+     #
+    
 
     def __str__(self):
         return self.department_name
@@ -50,6 +54,11 @@ class Nature(models.Model):
 
     def __str__(self):
         return self.Nature_name
+
+#class Department_Person(models.Model):
+   # DepartmentPerson= models.ForeignKey(Department, on_delete=models.SET_NULL, null=True,related_name='DepartmentPerson')
+   # name = models.CharField(max_length=300)
+   # email = models.EmailField(max_length=300)
 
 
 
