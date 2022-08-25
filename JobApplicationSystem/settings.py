8989ed98344dfa1e14d7731222c01369bfafc223
Aref Al Hamad <AAlHamad@msgroup.local>
2022-08-25 11:36:23 +0300

    add nginx and it work

--- JobApplicationSystem/settings.py
+++ JobApplicationSystem/settings.py
@@ -189,15 +189,24 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(TEMPLATES_DIR, 'static')] 
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates/static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIAFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
+
+
+
+
+
+
+
 
   
 # allows to load iframe from same hostname
